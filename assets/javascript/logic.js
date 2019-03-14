1d31b5b4b1ff7b694989ca0a3b812da49878d2fe
bigbuckalex <bigbuckalex@gmail.com>
2019-03-14 12:59:06 -0400

    added firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,18 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDGCTv9tkR6r9w-0P9Gz__Xi1FHnAkTAXU",
+    authDomain: "loose-cooking.firebaseapp.com",
+    databaseURL: "https://loose-cooking.firebaseio.com",
+    projectId: "loose-cooking",
+    storageBucket: "loose-cooking.appspot.com",
+    messagingSenderId: "535521502364"
+};
+firebase.initializeApp(config);
+
 // $.ajax({
 //     url: "https://www.food2fork.com/api/get?key=2f1201dc25f3e453a5c909d94d149d44&rId=35382",
 //     method: "GET"
 // }).then(function(response){
 //     console.log(response);
 // })
+
