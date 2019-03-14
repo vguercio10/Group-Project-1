def678b3089bf2ab246c68d5c54155052b94884d
bigbuckalex <bigbuckalex@gmail.com>
2019-03-14 11:19:01 -0400

    ajax request and links for bootstrap and jquery

---
+++ assets/javascript/logic.js
@@ -0,0 +1,6 @@
+// $.ajax({
+//     url: "https://www.food2fork.com/api/get?key=2f1201dc25f3e453a5c909d94d149d44&rId=35382",
+//     method: "GET"
+// }).then(function(response){
+//     console.log(response);
+// })
