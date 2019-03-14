dcd814a22adcb4ca391d01a21bfa6f18447f2188
plusultra <jideamoran@gmail.com>
2019-03-14 14:40:14 -0400

    template for web page, have css&js as well

---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
