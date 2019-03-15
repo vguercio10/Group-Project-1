793f48f4184593fdc5bc53d3da1621f42f0582f6
bigbuckalex <bigbuckalex@gmail.com>
2019-03-15 10:35:00 -0400

    merged javascript and css files and deleted unneeded files

--- main.js
+++
@@ -1,6 +0,0 @@
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
