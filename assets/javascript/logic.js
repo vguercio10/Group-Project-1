10cff6be03ad0766b38ac9e3f7026a29cf72b816
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-03-15 13:30:57 -0400

    fixed carousel

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
+    var instances = M.Carousel.init(elems, 'options');
 });
 
 // Initialize Firebase
